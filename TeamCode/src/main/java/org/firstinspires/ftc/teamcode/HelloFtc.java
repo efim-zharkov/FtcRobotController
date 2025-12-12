2d60ac2f476ddaaeb80d2dd6c8d4c426de40df5b
Miko <fima.zharkov@gmail.com>
2025-12-12 23:48:38 +0200

    update
    1.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloFtc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloFtc.java
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+
+@Disabled
 @TeleOp
 public class HelloFtc extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Efim!");
+
+    }
+
+    public void loop() {
+    }
+
+
 }
